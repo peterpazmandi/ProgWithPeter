41b28e34e1d820d960554d41ae2bfa577730775f
inspire-coding <inspire.coding.apps@gmail.com>
2022-02-21 16:06:56 +0100

    Show sections and lectures and update them

--- Client/src/app/section/upsert-sections-and-lectures-list/upsert-sections-and-lectures-list.component.ts
+++ Client/src/app/section/upsert-sections-and-lectures-list/upsert-sections-and-lectures-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { Lecture } from 'src/app/_models/lectureDto.model';
 import { Section } from 'src/app/_models/sectionDto.model';
 import { SectionsAndLecturesFormService } from './sections-and-lectures-form.service';
 
@@ -9,6 +10,7 @@ import { SectionsAndLecturesFormService } from './sections-and-lectures-form.ser
   styleUrls: ['./upsert-sections-and-lectures-list.component.css']
 })
 export class UpsertSectionsAndLecturesListComponent implements OnInit {
+  sections: Section[] = [];
 
   constructor(
     private fb: FormBuilder,
@@ -16,11 +18,24 @@ export class UpsertSectionsAndLecturesListComponent implements OnInit {
 
   ngOnInit(): void {
     this.initializeForms();
+
+    let sub = this.sectionsAndLecturesFormService.sectionsAndLecturesFrom.get('sections')?.valueChanges?.subscribe((value: Section[]) => {
+      this.sections.push(...(value as Section[]));
+      console.log(value);
+      sub?.unsubscribe();
+    })
   }
 
   private initializeForms() {
     this.sectionsAndLecturesFormService.sectionsAndLecturesFrom = this.fb.group({
       sections: [[] as Section[]]
+    });
+  }
+
+  onLectureChangeEvent(event: any, sectionIndex: number, lectureIndex: number) {
+    this.sections[sectionIndex].lectures[lectureIndex].post.title = event.target.value;
+    this.sectionsAndLecturesFormService.sectionsAndLecturesFrom.patchValue({
+      sections: this.sections
     })
   }
 
