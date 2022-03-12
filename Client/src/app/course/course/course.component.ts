1d86cf58d038f1df4aaab14fd5b97fcf6d0f86db
Peter Pazmandi <inspire.coding.apps@gmail.com>
2022-03-12 22:26:13 +0100

    Client: first impl. course comp.

--- Client/src/app/course/course/course.component.ts
+++ Client/src/app/course/course/course.component.ts
@@ -6,6 +6,7 @@ import { Course } from 'src/app/_models/courseDto.model';
 import { User } from 'src/app/_models/user.model';
 import { AccountService } from 'src/app/_services/account.service';
 import { CourseService } from 'src/app/_services/course.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-course',
@@ -31,21 +32,53 @@ export class CourseComponent implements OnInit {
   ngOnInit(): void {
     this.loadCurrentUser();
     this.loadCourse();
+
+    this.scrollToTop();
   }
 
   loadCourse() {
     let re =/\_/gi;
     let title = this.route.url.split('/')[2].replace(re, ' ');
-    this.courseService.getCourseByTitle(title, this.currentUser.id).subscribe(result => {
-      this.course = result
+    this.courseService.getCourseByTitle(title, this.currentUser.id).subscribe(response => {
+      this.course = response
+      console.log(this.course);
+
+      this.updateMeta()
     }, error => {
       console.error(error);
     })
+  }
+
+  private updateMeta() {
+    this.meta.updateTag({name: "title", content: this.course.post.meta?.seoTitle || ''})
+    this.meta.updateTag({name: "description", content: this.course.post.meta?.metaDescription || ''})
+    this.meta.updateTag({name: "image", content: this.course.post.featuredImageUrl})
+    this.meta.updateTag({name: "site", content: environment.siteTitle})
   }
 
   loadCurrentUser() {
     this.accountService.currentUser$.subscribe(user => {
       this.currentUser = user;
     })
+  }
+
+  private scrollToTop() {
+    window.scrollTo(0, 0);
+  }
+  
+  hideSidebar() {
+    this.sideBarVisible = !this.sideBarVisible;
+
+    if(this.sideBarVisible) {
+      this.sidebarWidth = 'col-3';
+      this.conetentWidth = 'col-9';
+    } else {
+      this.sidebarWidth = 'col-1';
+      this.conetentWidth = 'col';
+    }
+  }
+
+  updateContent(event: string) {
+    this.course.post.content = event;
   }
 }
