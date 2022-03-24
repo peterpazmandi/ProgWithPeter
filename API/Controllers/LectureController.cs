6af9fb5c6d9ea5a250160a10ee7dc891da654f68
inspire-coding <inspire.coding.apps@gmail.com>
2022-03-24 08:17:30 +0100

    API: get back course comp. rate, SetLectureCompletion endpoint

--- API/Controllers/LectureController.cs
+++ API/Controllers/LectureController.cs
@@ -41,18 +41,23 @@ namespace API.Controllers
 
         [Authorize(Roles = "Admin, Moderator")]
         [HttpPost("SetLectureCompletion")]
-        public async Task<ActionResult> SetLectureCompletion(int lectureId, bool isCompleted)
+        public async Task<ActionResult<double>> SetLectureCompletion(int lectureId, bool isCompleted)
         {
             string username = User.GetUsername();
             var user = await _unitOfWork.UserRepository.GetUserByUsernameAsync(username);
 
             var lecture = await _unitOfWork.LectureRepository.FindLectureById(lectureId);
 
+            if(lecture == null)
+            {
+                return NotFound($"Lecture with Id {lectureId} not found!");
+            }
+
             await _unitOfWork.LectureActivityRepository.SetLectureCompletion(lecture, user, isCompleted);
 
             if(await _unitOfWork.Complete())
             {
-                return Ok();
+                return Ok(await _unitOfWork.CourseRepository.GetCourseProgressByLectureId(lectureId, user.Id));
             }
             
             return BadRequest("Operation failed!");
