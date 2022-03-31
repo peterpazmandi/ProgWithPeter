17fe0cb6f7f3cc1f1f5a1c9e768a7147179ab1b9
Peter Pazmandi <inspire.coding.apps@gmail.com>
2022-03-31 22:09:47 +0200

    Client: update completed lectures' color

--- Client/src/app/course/course/course-content/course-content.component.ts
+++ Client/src/app/course/course/course-content/course-content.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { LectureActivity } from 'src/app/_models/lectureActivityDto.model';
 import { Section } from 'src/app/_models/sectionDto.model';
 import { User } from 'src/app/_models/user.model';
 import { AccountService } from 'src/app/_services/account.service';
@@ -28,15 +29,29 @@ export class CourseContentComponent implements OnInit {
     })
   }
 
-  handleClick(checkbox: Event) {
-    let id = parseInt((<HTMLInputElement>checkbox.target).id);
+  handleClick(sectionIndex: number, checkbox: Event) {
+    const lectureIndex = parseInt((<HTMLInputElement>checkbox.target).id);
+    const lectureIsCompleted = (<HTMLInputElement>checkbox.target).checked;
+    
+    this.updateLectureActivity(sectionIndex, lectureIndex, lectureIsCompleted);
+    
     this.lectureService.setLectureCompletion(
-      parseInt((<HTMLInputElement>checkbox.target).id),
-      (<HTMLInputElement>checkbox.target).checked
+      this.sections[sectionIndex].lectures[lectureIndex].id,
+      lectureIsCompleted
     ).subscribe(completionRate => {
       this.courseCompletionRate.emit(completionRate as number);
     }, error => {
       console.log(error);
     })
+  }
+
+  private updateLectureActivity(sectionIndex: number, lectureIndex: number, lectureIsCompleted: boolean) {
+    if(this.sections[sectionIndex].lectures[lectureIndex].lectureActivities !== undefined) {
+      this.sections[sectionIndex].lectures[lectureIndex].lectureActivities[0].isCompleted = lectureIsCompleted;
+    } else {
+      this.sections[sectionIndex].lectures[lectureIndex].lectureActivities.push({
+        isCompleted: lectureIsCompleted
+      } as LectureActivity)
+    }
   }
 }
